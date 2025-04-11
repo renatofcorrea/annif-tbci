d504fc28767ec3e11a947727cf55875a3e498b42
renatofcorrea <renatofcorrea@users.noreply.github.com>
2025-04-11 20:55:48 +0000

    Configuração do app

--- run-script.sh
+++ run-script.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
+# Ativa o ambiente virtual
 source annif-venv/bin/activate
+
+# Instala dependências
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Navega para o diretório do ambiente virtual
 cd annif-venv
+
+# Ativa o ambiente virtual
 source bin/activate
+
+# Executa o servidor na porta 8000 e o torna público
 annif run --host 0.0.0.0 --port 8000
