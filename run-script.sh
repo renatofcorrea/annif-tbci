228a8e0b387294f9527600c72c381629f727172a
renatofcorrea <renatofcorrea@users.noreply.github.com>
2026-04-17 19:38:25 +0000

    modified:   run-script.sh

--- run-script.sh
+++ run-script.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 # Ativa o ambiente virtual
-source annif-venv/bin/activate
+#source annif-venv/bin/activate
+# Ativa venv se existir
+if [ -d "annif-venv" ]; then
+  source annif-venv/bin/activate
+fi
 
 # Instala dependências
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Garante que Annif está instalado
+pip install annif flask uvicorn
+
 # Navega para o diretório do ambiente virtual
 cd annif-venv
 
 # Ativa o ambiente virtual
-source bin/activate
+#source bin/activate
 
 # Executa o servidor na porta 8000 e o torna público (comando no app.py)
 annif run --host 0.0.0.0 --port 8000
-
-# Execute o aplicativo Python (app.py)
-#python ../app.py
